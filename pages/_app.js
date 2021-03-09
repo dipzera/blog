0e88c3b4c5cb7e852a01bcae3c63d4537db26f0e
dipzera <nathanielthomaz@gmail.com>
2021-03-09 16:08:05 +0200

    make overall layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,26 @@
 import Link from "next/link";
+import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto my-8 w-9/12">
+      <header>
+        <h1 className="text-6xl font-bold text-center">My blog</h1>
+        <nav>
+          <ul className="flex flex-row space-x-4 justify-center">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </div>
   );
